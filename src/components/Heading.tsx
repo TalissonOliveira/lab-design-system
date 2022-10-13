47c425c2e832f4d8cd0abf355b0773cb96d15328
Talisson Oliveira <talissonlux@outlook.com>
2022-10-13 03:52:07 -0300

    feat: creates heading component with storybook

---
+++ src/components/Heading.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from 'react'
+import { Slot } from '@radix-ui/react-slot'
+import { clsx } from 'clsx'
+
+export interface HeadingProps {
+  size?: 'sm' | 'md' | 'lg',
+  asChild?: boolean
+  children: ReactNode
+}
+
+export function Heading({ size = 'md', asChild, children }: HeadingProps) {
+  const Comp = asChild ? Slot : 'h2'
+
+  return (
+    <Comp className={clsx(
+      'text-gray-100 font-sans font-bold', {
+        'text-lg': size === 'sm',
+        'text-xl': size === 'md',
+        'text-2xl': size === 'lg'
+      }
+    )}>
+      {children}
+    </Comp>
+  )
+}
