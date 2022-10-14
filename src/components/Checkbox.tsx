a5725935569f41c1fef2746b89d6d8b2f57dcdbb
Talisson Oliveira <talissonlux@outlook.com>
2022-10-14 07:57:34 -0300

    feat: extends checkbox properties and passes to component

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,11 +1,11 @@
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
 import { Check } from 'phosphor-react'
 
-export interface CheckboxProps {}
+export interface CheckboxProps extends CheckboxPrimitive.CheckboxProps {}
 
-export function Checkbox({ }: CheckboxProps) {
+export function Checkbox({ ...rest }: CheckboxProps) {
   return (
-    <CheckboxPrimitive.Root className="bg-gray-800 w-6 h-6 p-[2px] rounded">
+    <CheckboxPrimitive.Root className="bg-gray-800 w-6 h-6 p-[2px] rounded" {...rest}>
       <CheckboxPrimitive.Indicator asChild>
         <Check weight="bold" className="w-5 h-5 text-cyan-500" />
       </CheckboxPrimitive.Indicator>
