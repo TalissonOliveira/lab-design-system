d89a9021b106b5bce751f08086a5ea4324cf7fd4
Talisson Oliveira <talissonlux@outlook.com>
2022-10-15 00:35:06 -0300

    feat: adds login page to storybook

---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { SignIn } from './SignIn'
+
+export default {
+  title: 'Components/SignIn',
+  component: SignIn,
+  args: {},
+  argTypes: {}
+} as Meta
+
+export const Default: StoryObj = {}
